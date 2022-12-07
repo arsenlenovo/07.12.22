a8f05e670be70a2d7b1e99be74bf67b06e2e6d23
arsenlenovo <arsenarsarsenars85@gmail.com>
2022-12-07 20:51:51 +0600

    while

---
+++ app.js
@@ -0,0 +1,12 @@
+let quantity = 200;
+let body = document.querySelector('body');
+let index = 0;
+while (index < quantity) {
+    let drop = document.createElement('index');
+    
+    body.appendChild(drop)
+    index++
+     
+}
+  
+
